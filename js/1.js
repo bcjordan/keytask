9ac07d9e4dcdb6dbb67e0362edb69b034fc4c759
Brian Jordan <bcjordan@gmail.com>
2011-11-29 09:43:19 -0500

    task logging maturing

--- js/1.js
+++ js/1.js
@@ -1,2 +1,36 @@
 var a = 5;
-console.log("A is " + a);
+
+function newPrompt(){
+	// Immediately generate, display, and log new prompt
+	var newNumber = 65 + Math.floor(Math.random()*25);
+	var newChar   =  String.fromCharCode(newNumber);
+	
+	$('#prompt').text(newChar);
+	
+	$('#log').append('<br/>' + +new Date() + ", prompt, " + newChar);
+}
+
+$('#input').keydown(
+    function(e) {
+				// Log key press
+        console.log(e.timeStamp);
+        console.log(e.keyCode);
+				$('#log').append('<br/>' + e.timeStamp + ", press, " + e.keyCode);
+				
+				newPrompt();
+    }
+);
+
+$('#input').keyup(
+    function(e) {
+				$('#input').val('');
+    }
+);
+
+$('#input').click(
+	function(e) {
+		$('#input').val('');
+		// Initiate task
+		newPrompt();
+	}
+);
