d3c721e3d5d496238bb428dd2993d0f15bcead72
Brian Jordan <bcjordan@gmail.com>
2011-12-07 19:42:43 -0500

    add hash of task data

--- js/1.js
+++ js/1.js
@@ -1,23 +1,30 @@
-var a = 5;
 var currChar;
+var currStart;
+var keyPresses = new Array();
+
+
 function newPrompt(){
 	// Immediately generate, display, and log new prompt
 	var newNumber = 65 + Math.floor(Math.random()*25);
-	currChar      =  String.fromCharCode(newNumber);
-	
+	currChar      = String.fromCharCode(newNumber);
+	currStart     = +new Date()
 	$('#prompt').text(currChar);
 	
-	$('#log').prepend('<div class="alert-message log-entry ">' + +new Date() + ", prompt, " + currChar + "</div>");
+	$('#log').prepend('<div class="alert-message log-entry ">' + currStart + ", prompt, " + currChar + "</div>");
 }
 
 $('#input').keypress(
     function(e) {
 				// Log key press
+				var keyCode = e.keyCode || e.which
         console.log(e.timeStamp);
-        console.log(e.keyCode);
-				var pressed = String.fromCharCode(e.keyCode || e.which).toUpperCase();
+        console.log(keyCode);
+				var pressed = String.fromCharCode(keyCode).toUpperCase();
 				var correctness = (currChar == pressed) ? "success" : "error"
 				$('#log').prepend('<div class="alert-message log-entry '+ correctness +'">' + e.timeStamp + ", press, " + pressed + "</div>");
+				
+				keyPresses.push({key: pressed, prompt: currChar, correct: (correctness === "success"),
+								         start: currStart, end: e.timeStamp, delay: (e.timeStamp - currStart)});
 				
 				newPrompt();
     }
@@ -36,3 +43,4 @@ $('#input').click(
 		}
 	}
 );
+
